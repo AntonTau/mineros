2ba1d0051b668acba9dcfc4d65a4e6fa6e689a2f
Anton Tausvik <antontausvik@gmail.com>
2025-10-03 15:11:31 +0200

    fix: Added inventory client

--- src/anton_test/src/anton_place_block_node.cpp
+++ src/anton_test/src/anton_place_block_node.cpp
@@ -1,23 +1,37 @@
+#include <geometry_msgs/msg/detail/pose_stamped__struct.hpp>
+#include <memory>
+#include <mineros_inter/srv/detail/inventory__struct.hpp>
 #include <rclcpp/rclcpp.hpp>
 
 // Service
 #include <mineros_inter/srv/place_block.hpp>
+#include <mineros_inter/srv/simple_place_block.hpp>
+#include <mineros_inter/srv/inventory.hpp>
 
 // Messages
 #include <geometry_msgs/msg/pose.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
 #include <geometry_msgs/msg/point.hpp>
 #include <mineros_inter/msg/block_pose.hpp>
 #include <mineros_inter/msg/item.hpp>
 
 
+
 using namespace std::chrono_literals;
 
-class PlaceBlock : public rclcpp::Node
+class PlaceBlockNode : public rclcpp::Node
 {
 public:
-     PlaceBlock() : Node("place_block")
+     PlaceBlockNode() : Node("place_block")
     {
+        // Add build date for sanity checks.
+		RCLCPP_INFO(this->get_logger(), "Running PlaceBlockNode built on %s at %s", __DATE__, __TIME__);
+
+
         client_ = this->create_client<mineros_inter::srv::PlaceBlock>("/mineros/interaction/place_block");
+        inventory_client_= this->create_client<mineros_inter::srv::Inventory>("/mineros/inventory/contents");
+
+
 
         while (!client_->wait_for_service(1s)) {
             RCLCPP_INFO(this->get_logger(), "Venter på place_block service");
@@ -26,16 +40,16 @@ public:
         auto request = std::make_shared<mineros_inter::srv::PlaceBlock::Request>();
 
         // Block pose and orientation
-        request->block.block_pose.position.x = 119;
-        request->block.block_pose.position.y = 101;
-        request->block.block_pose.position.z = 126;
+        request->block.block_pose.position.x = 115;
+        request->block.block_pose.position.y = 103;
+        request->block.block_pose.position.z = 123;
         request->block.block_pose.orientation.w = 1;
         
         // Item id
         request->block.block.id = 22;
 
         // Vector orient
-        request->block.face_vector.y = 1;
+        request->block.face_vector.z = -1;
 
 
         try
@@ -63,16 +77,26 @@ public:
         
     }
 
-private:
-    rclcpp::Client<mineros_inter::srv::PlaceBlock>::SharedPtr client_;
 
     
+private:
+    rclcpp::Client<mineros_inter::srv::PlaceBlock>::SharedPtr client_;
+    rclcpp::Service<mineros_inter::srv::SimplePlaceBlock>::SharedPtr placeblock_service_;
+    rclcpp::Client<mineros_inter::srv::Inventory>::SharedPtr inventory_client_;
+
+    void placeBlockCallback(const std::shared_ptr<mineros_inter::srv::SimplePlaceBlock::Request> request,
+    std::shared_ptr<mineros_inter::srv::SimplePlaceBlock::Response> response) {
+
+        auto inventory_request = std::make_shared<mineros_inter::srv::Inventory::Request>();
+
+    }
+
 };
 
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
-  std::make_shared<PlaceBlock>();
+  std::make_shared<PlaceBlockNode>();
   rclcpp::shutdown();
   return 0;
 }
